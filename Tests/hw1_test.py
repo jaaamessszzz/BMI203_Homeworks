f33ff5aaf8e07a0cb935400be1323da833f85c7e
James Lucas <james.lucas@berkeley.edu>
2017-01-22 23:52:56 -0800

    Travis CI r u srs.

--- Tests/hw1_test.py
+++ Tests/hw1_test.py
@@ -2,27 +2,27 @@ import numpy as np
 from Homework1.Quicksort_Bubblesort import Sorting_Algorithms
 
 def test_bubblesort():
-    x = [1, 2, 4, 0, 6, 8, 10, 7, 5, 3, 9]
-    sorted = Sorting_Algorithms.Bubblesort(x)
+    unsorted_list = [1, 2, 4, 0, 6, 8, 10, 7, 5, 3, 9]
+    sorted = Sorting_Algorithms.Bubblesort(unsorted_list)
     assert sorted == [0,1,2,3,4,5,6,7,8,9,10]
 
-    x.append("A")
-    sorted = Sorting_Algorithms.Bubblesort(x)
+    unsorted_list.append("A")
+    sorted = Sorting_Algorithms.Bubblesort(unsorted_list)
     assert sorted == "List contains an non-int or -float element!"
 
-    x = []
-    sorted = Sorting_Algorithms.Bubblesort(x)
+    unsorted_list = []
+    sorted = Sorting_Algorithms.Bubblesort(unsorted_list)
     assert sorted == "Unsorted list is empty!"
 
 def test_quicksort():
-    x = [1, 2, 4, 0, 6, 8, 10, 7, 5, 3, 9]
-    sorted = Sorting_Algorithms.Quicksort(x)
+    unsorted_list = [1, 2, 4, 0, 6, 8, 10, 7, 5, 3, 9]
+    sorted = Sorting_Algorithms.Quicksort(unsorted_list)
     assert sorted == [0,1,2,3,4,5,6,7,8,9,10]
 
-    x.append("A")
-    sorted = Sorting_Algorithms.Bubblesort(x)
+    unsorted_list.append("A")
+    sorted = Sorting_Algorithms.Bubblesort(unsorted_list)
     assert sorted == "List contains an non-int or -float element!"
 
-    x = []
-    sorted = Sorting_Algorithms.Bubblesort(x)
+    unsorted_list = []
+    sorted = Sorting_Algorithms.Bubblesort(unsorted_list)
     assert sorted == "Unsorted list is empty!"
