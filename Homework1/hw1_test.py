8ab57494d550c73f81b58bdcce4b800fa4dfe188
James Lucas <james.lucas@berkeley.edu>
2017-01-22 23:21:23 -0800

    Added checks, writing tests. Opted for if statements with returned strings in place of asserts for ease of testing. Still need to learn how to use Travis CI more effectively...

---
+++ Homework1/hw1_test.py
@@ -0,0 +1,28 @@
+import numpy as np
+from Quicksort_Bubblesort import Sorting_Algorithms
+
+def test_bubblesort():
+    x = [1, 2, 4, 0, 6, 8, 10, 7, 5, 3, 9]
+    sorted = Sorting_Algorithms.Bubblesort(x)
+    assert sorted == [0,1,2,3,4,5,6,7,8,9,10]
+
+    x.append("A")
+    sorted = Sorting_Algorithms.Bubblesort(x)
+    assert sorted == "List contains an non-int or -float element!"
+
+    x = []
+    sorted = Sorting_Algorithms.Bubblesort(x)
+    assert sorted == "Unsorted list is empty!"
+
+def test_quicksort():
+    x = [1, 2, 4, 0, 6, 8, 10, 7, 5, 3, 9]
+    sorted = Sorting_Algorithms.Quicksort(x)
+    assert sorted == [0,1,2,3,4,5,6,7,8,9,10]
+
+    x.append("A")
+    sorted = Sorting_Algorithms.Bubblesort(x)
+    assert sorted == "List contains an non-int or -float element!"
+
+    x = []
+    sorted = Sorting_Algorithms.Bubblesort(x)
+    assert sorted == "Unsorted list is empty!"
