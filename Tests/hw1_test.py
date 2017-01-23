8732d63bf1e89125338fc2faac3fdcac26d2672c
James Lucas <james.lucas@berkeley.edu>
2017-01-23 10:03:24 -0800

    ...

--- Tests/hw1_test.py
+++ Tests/hw1_test.py
@@ -5,32 +5,35 @@ def test_bubblesort():
     unsorted_list = [1, 2, 4, 0, 6, 8, 10, 7, 5, 3, 9]
     sort_stuff = Sorting_Algorithms()
 
-    sorted = sort_stuff.Bubblesort(unsorted_list=unsorted_list)
-    print(sorted)
-    type(sorted)
-    assert sorted == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    my_sorted_list = sort_stuff.Bubblesort(unsorted_list=unsorted_list)
+    print(my_sorted_list)
+    print(type(my_sorted_list))
+    assert my_sorted_list == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
     unsorted_list.append("A")
-    sorted = sort_stuff.Bubblesort(unsorted_list=unsorted_list)
-    assert sorted == "List contains an non-int or -float element!"
+    my_sorted_list = sort_stuff.Bubblesort(unsorted_list=unsorted_list)
+    assert my_sorted_list == "List contains an non-int or -float element!"
 
     unsorted_list = []
-    sorted = sort_stuff.Bubblesort(unsorted_list=unsorted_list)
-    assert sorted == "Unsorted list is empty!"
+    my_sorted_list = sort_stuff.Bubblesort(unsorted_list=unsorted_list)
+    assert my_sorted_list == "Unsorted list is empty!"
 
 def test_quicksort():
     unsorted_list = [1, 2, 4, 0, 6, 8, 10, 7, 5, 3, 9]
     sort_stuff = Sorting_Algorithms()
 
-    sorted = sort_stuff.Quicksort(unsorted_list=unsorted_list)
-    print(sorted)
-    type(sorted)
-    assert sorted == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    my_sorted_list = sort_stuff.Quicksort(unsorted_list=unsorted_list)
+    print(my_sorted_list)
+    print(type(my_sorted_list))
+    assert my_sorted_list == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
     unsorted_list.append("A")
-    sorted = sort_stuff.Quicksort(unsorted_list=unsorted_list)
-    assert sorted == "List contains an non-int or -float element!"
+    my_sorted_list = sort_stuff.Quicksort(unsorted_list=unsorted_list)
+    assert my_sorted_list == "List contains an non-int or -float element!"
 
     unsorted_list = []
-    sorted = sort_stuff.Quicksort(unsorted_list=unsorted_list)
-    assert sorted == "Unsorted list is empty!"
+    my_sorted_list = sort_stuff.Quicksort(unsorted_list=unsorted_list)
+    assert my_sorted_list == "Unsorted list is empty!"
+
+test_bubblesort()
+test_quicksort()
